f4bca9fe984611b111a80c08767d4c8770ae633f
DiegoPSantana <diego.fourstage@gmail.com>
2021-02-01 11:21:29 -0300

    Game Loop (FPS)

--- src/com/zelda/game/GamePanel.java
+++ src/com/zelda/game/GamePanel.java
@@ -1,9 +1,8 @@
 package com.zelda.game;
 
 import javax.swing.JPanel;
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -42,24 +41,87 @@ public class GamePanel extends JPanel implements Runnable {
     public void run(){
         init();
 
+        final double GAME_HERTZ = 60.0;
+        final double TBU = 1000000000 / GAME_HERTZ; // Tempo antes da atualização
+
+        final int MUBR = 5; // Deve atualizar antes da renderização
+
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+
+        final double TARGET_FPS = 60;
+        final double TTBR = 1000000000 / TARGET_FPS; // Tatal de tempo antes da renderização
+
+        int frameCount = 0;
+        int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+        int oldFrameCount = 0;
+
+
         while(running){
+            double now = System.nanoTime();
+            int updateCount = 0;
+
+            while (((now - lastUpdateTime) > TBU) && (updateCount < MUBR)){
+                update();
+                input();
+                lastUpdateTime += TBU;
+                updateCount++;
+            }
+
+            if(now - lastUpdateTime > TBU){
+                lastUpdateTime = now -TBU;
+            }
+            input();
             update();
             render();
             draw();
+            lastRenderTime = now;
+            frameCount++;
+
+            int thisSecond = (int) (lastUpdateTime / 1000000000);
+            if(thisSecond > lastSecondTime){
+                if(frameCount != oldFrameCount){
+                    System.out.println("NEW SECOND " + thisSecond + " " + frameCount);
+                    oldFrameCount = frameCount;
+                }
+
+                frameCount = 0;
+                lastSecondTime = thisSecond;
+            }
+
+            while(now - lastRenderTime < TTBR && now - lastUpdateTime < TBU){
+                Thread.yield();
+
+                try{
+                    Thread.sleep(1);
+                }catch(Exception e) {
+                    System.out.println("ERROR: yelding thread");
+                }
+                now = System.nanoTime();
+            }
+
         }
     }
 
-    private int x = 0;
-
     public void update(){
-    x++;
-    System.out.println(x);
+
+    }
+
+    public void input(){
+
     }
 
     public void render(){
+        if(g != null){
+            g.setColor(new Color(66, 134, 244) );
+            g.fillRect(0, 0, width, height);
+        }
 
     }
     public void draw(){
+        Graphics g2 = (Graphics) this.getGraphics();
+        g2.drawImage(img,0,0, width, height, null);
+        g2.dispose();
 
     }
     // stop the minutes 12:03
