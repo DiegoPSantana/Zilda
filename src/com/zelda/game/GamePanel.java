8c24fd1c74fc3f1a7b398b274d6b447ceec5a40d
DiegoPSantana <diego.fourstage@gmail.com>
2021-02-01 09:41:27 -0300

    Thread, Graphics2D

--- src/com/zelda/game/GamePanel.java
+++ src/com/zelda/game/GamePanel.java
@@ -2,18 +2,66 @@ package com.zelda.game;
 
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     public static int width;
     public static int height;
 
+    private Thread thread;
+    private BufferedImage img;
+    private Graphics2D g;
+    private boolean running = false;
+
     public GamePanel(int width, int height){
+        this.width = width;
+        this.height = height;
         setPreferredSize(new Dimension(width, height));
         setFocusable(true);
         requestFocus();
     }
 
+    public void addNotify(){
+        super.addNotify();
+
+        if(thread == null){
+            thread = new Thread(this, "GameThread");
+            thread.start();
+        }
+    }
+
+    public void init(){
+        running = true;
+
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        g = (Graphics2D) img.getGraphics();
+    }
+
+    public void run(){
+        init();
+
+        while(running){
+            update();
+            render();
+            draw();
+        }
+    }
+
+    private int x = 0;
+
+    public void update(){
+    x++;
+    System.out.println(x);
+    }
+
+    public void render(){
+
+    }
+    public void draw(){
+
+    }
     // stop the minutes 12:03
 
 }
